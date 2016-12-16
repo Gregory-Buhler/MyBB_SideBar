c36c183af867731e81b5e65355d034f7c41b612b
Gregory Buhler <Gregory@GregBWebDev.Tech>
2016-12-15 22:08:41 -0600

    Setting added for choosing side, setting is now passed into javascript functions for resizing.

--- MyBB_SideBar.php
+++ MyBB_SideBar.php
@@ -21,17 +21,13 @@ function MyBB_SideBar_info(){
   );
 }
 
-/**
-  *
-  *
-  *
-**/
+
 function MyBB_SideBar_activate() {
   global $db;
 
   $MyBB_SideBar_group = array(
     'gid'             => 'NULL',
-    'name'            => 'MyBBSDB',
+    'name'            => 'MyBB_SideBar',
     'title'           => 'MyBB_SideBar Plugin',
     'description'     => 'MyBB_SideBar plugin that inserts a MyBB_SideBar on either the left/right/both side(s) of the forum.',
     'disporder'       => "1",
@@ -53,12 +49,26 @@ function MyBB_SideBar_activate() {
   );
 
   $db->insert_query('settings', $MyBB_SideBar_setting);
+
+  $MyBB_Sidebar_setting_side = array(
+    'sid'             => 'NULL',
+    'name'            => 'MyBB_SideBar_plugin_side',
+    'title'           => 'Which side for the SideBar?',
+    'description'     => 'This option chooses which side the sidebar shows on.',
+    'optionscode'     => 'select \n 1=Right \n 2=Left \n 3=both',
+    'value'           => '1',
+    'disporder'       => 2,
+    'gid'             => intval($gid),
+  );
+
+  $db->insert_query('settings', $MyBB_Sidebar_setting_side);
   rebuild_settings();
 }
 
 function MyBB_SideBar_deactivate() {
   global $db;
   $db->query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN ('MyBB_SideBar_plugin_enable')");
+  $db->query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN ('MyBB_SideBar_plugin_side')");
   $db->query("DELETE FROM " . TABLE_PREFIX . "settinggroups WHERE name='MyBB_SideBar'");
   rebuild_settings();
 }
@@ -69,6 +79,9 @@ function MyBB_SideBar_global_start(){
   if ($mybb->settings['MyBB_SideBar_plugin_enable'] == 1) {
     echo "MyBB_SideBar plugin enabled! It Works! HOORAH";
     include 'MyBB_SideBar/forum_resize.js';
+    echo "<script type='text/javascript'>
+      addMainDiv(" . $mybb->settings['MyBB_SideBar_plugin_side'] . ");
+      </script>";
   }
 }
 ?>
--- MyBB_SideBar/forum_resize.js
+++ MyBB_SideBar/forum_resize.js
@@ -1,10 +1,17 @@
 <script type="text/javascript">
-  document.addEventListener('DOMContentLoaded', function() {
-    var forum = document.getElementsByClassName("wrapper");
-    console.log(forum);
-    var forumWrapper = forum[3];
-    forumWrapper.innerHTML = "<div id=\"main\">" + forumWrapper.innerHTML +  "</div>";
+  function addMainDiv(boardSide) {
+    document.addEventListener('DOMContentLoaded', function() {
+      var forum = document.getElementsByClassName("wrapper");
+      console.log(forum);
+      var forumWrapper = forum[3];
+      forumWrapper.innerHTML = "<div id=\"main\">" + forumWrapper.innerHTML +  "</div>";
+      resizeForum(boardSide);
+    }, false);
+  }
+
+  function resizeForum(boardSide) {
+    console.log(boardSide);
     var main = document.getElementById("main");
     main.style.width = "80%";
-  }, false);
+  }
 </script>
